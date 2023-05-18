bd253033fa914bf592d8f8e09b3e236c7555fee0
Ramazuki <ramazukigamer@gmail.com>
2023-05-18 14:29:03 +0300

    errors are now returned as well + some minor changes in diagonal check algo + matrixCheck

--- src/CompMathLab1/CodeTest/Result.java
+++ src/CompMathLab1/CodeTest/Result.java
@@ -3,13 +3,28 @@ package CompMathLab1.CodeTest;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import static java.util.Collections.swap;
 
 public class Result {
     public static boolean isMethodApplicable = true;
     public static String errorMessage;
 
+    public static void ruleCheck (int n, List<List<Double>> matrix){
+        matrixCheck(n, matrix);
+        if(isMethodApplicable)
+            diagonalCheck(n, matrix);
+    }
 
+    public static void matrixCheck(int n, List<List<Double>> matrix){
+        errorMessage = "Input data cannot be considered as matrix";
+        int expectedRowSize = matrix.get(0).size();
+        for (int i = 1; i < n; i++) {
+            int curRowSize = matrix.get(i).size();
+            if (curRowSize != expectedRowSize) {
+                isMethodApplicable = false;
+                break;
+            }
+        }
+    }
     public static void diagonalCheck (int n, List<List<Double>> matrix){
         for (int i = 0; i < n; i++) {
             double A = matrix.get(i).get(i);
@@ -22,20 +37,18 @@ public class Result {
                 List<Double> row = matrix.get(i);
                 //Trying to find numbers to solve diagonal predominance problem
                 boolean foundSolution = false;
-                for (int j = 0; j < n; j++) {
-                    double B = row.get(j);
-                    if(B != A){
-                        if (Math.abs(B) >= row.stream()
-                                .map(Math::abs)
-                                .reduce(0.0, Double::sum) - Math.abs(last) - Math.abs(B)) {
-                            swap(row, i, j);
-                            matrix.set(i, row);
-                            foundSolution = true;
-                            break;
-                        }
-                    }
+                row.remove(n);
+                double B = Collections.max(row);
+                row.add(last);
+                if(B != A && Math.abs(B) >= row.stream()
+                        .map(Math::abs)
+                        .reduce(0.0, Double::sum) - Math.abs(B) - Math.abs(last)) {
+                    int index = row.indexOf(B);
+                    Collections.swap(row, i, index);
+                    matrix.set(i, row);
+                    foundSolution = true;
                 }
-                if (!foundSolution){
+                if (!foundSolution) {
                     isMethodApplicable = false;
                     errorMessage = "The system has no diagonal dominance for this method. Method of the Gauss-Seidel is not applicable.";
                     break;
@@ -45,10 +58,11 @@ public class Result {
     }
 
     public static List<Double> solveByGaussSeidel (int n, List<List<Double>> matrix, double epsilon){
-        diagonalCheck(n, matrix);
+        ruleCheck(n, matrix);
         int b_place = n;
         List<Double> solutionVector = new ArrayList<>(Collections.nCopies(n, 0.0));
         List<Double> previous = new ArrayList<>(solutionVector);
+        List<Double> errors = new ArrayList<>(solutionVector);
         int itterations = 0;
 
         while (true) {
@@ -65,17 +79,20 @@ public class Result {
                 continue;
             boolean isOver = true;
             for (int i = 0; i < n; i++) {
-                if(Math.abs(solutionVector.get(i) - previous.get(i)) > epsilon){
+                double error = Math.abs(solutionVector.get(i) - previous.get(i)) / Math.abs(solutionVector.get(i));
+                errors.set(i, error);
+                if(errors.get(i) > epsilon){
                     isOver = false;
-                    break;
                 }
             }
             if(isOver)
                 break;
             previous = new ArrayList<>(solutionVector);
         }
+        List<Double> joinedVector = new ArrayList<>(solutionVector);
+        joinedVector.addAll(errors);
         System.out.println("iterations: " + itterations);
-        return solutionVector;
+        return joinedVector;
     }
 
 }
